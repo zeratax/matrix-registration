da7c973e0f1989b0a8b11365bc7ae1e445aa3634
ZerataX <mail@zera.tax>
2018-06-01 08:38:48 +0200

    change syntax classes

--- demo.js
+++ demo.js
@@ -67,17 +67,17 @@ function syntaxHighlight(json) {
     }
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
+        var cls = 'pl-c1'; // number
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
-                cls = 'key';
+                cls = 'pl-s'; // key
             } else {
-                cls = 'string';
+                cls = 'pl-s'; //string
             }
         } else if (/true|false/.test(match)) {
-            cls = 'boolean';
+            cls = 'pl-c1'; // boolean
         } else if (/null/.test(match)) {
-            cls = 'null';
+            cls = 'pl-c1'; // boolean
         }
         return '<span class="' + cls + '">' + match + '</span>';
     });
