40307d6f269763ef772d25389e7973e01eefa3af
ZerataX <mail@zera.tax>
2018-06-01 08:11:45 +0200

    pretty json

--- demo.js
+++ demo.js
@@ -61,6 +61,28 @@ since we just overwrite the submit button we don't hinder any functionality
 for users without javascript
 */
 
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 function sendData() {
     var XHR = new XMLHttpRequest();
 
@@ -69,9 +91,10 @@ function sendData() {
 
     // Define what happens on successful data submission
     XHR.addEventListener("load", function(event) {
-      var response = XHR.responseText
-      document.getElementById("response").innerHTML = response;
-      consonle.log(response);
+      var response = XHR.responseText;
+      console.log(response);
+      var str = JSON.stringify(response, null, 4);
+      document.getElementById("response").innerHTML = syntaxHighlight(str);
     });
 
     // Define what happens in case of error
