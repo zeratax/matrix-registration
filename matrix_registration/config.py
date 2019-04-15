e6cb6c40afc15fa8a16cfdd29dd137e5e6139e91
ZerataX <mail@zera.tax>
2019-04-16 01:58:14 +0200

    read config manually if file doesn't exist

--- matrix_registration/config.py
+++ matrix_registration/config.py
@@ -7,6 +7,9 @@ import sys
 # Third-party imports...
 import yaml
 
+# Local imports...
+from .constants import CONFIG_SAMPLE_PATH
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,16 +22,22 @@ class Config:
     def load(self):
         logger.debug('loading config...')
         dictionary = None
+        config_exists = True
         if type(self.data) is dict:
             logger.debug('from dict...')
             dictionary = self.data
         else:
             logger.debug('from file...')
+            if not os.path.isfile(self.data):
+                config_exists = False
+                self.data = CONFIG_SAMPLE_PATH
             try:
                 with open(self.data, 'r') as stream:
                     dictionary = yaml.load(stream, Loader=yaml.SafeLoader)
             except IOError as e:
                 sys.exit(e)
+        if not config_exists:
+            self.read_config(dictionary)
 
         logger.debug('setting config...')
         # recusively set dictionary to class properties
@@ -44,6 +53,18 @@ class Config:
         self.options = None
         self.load()
         logger.debug('config updated!')
+
+    def read_config(self, dictionary):
+        # important keys that need to be changed
+        keys = ['server_location', 'server_name', 'shared_secret', 'port']
+        for key in keys:
+            temp = dictionary[key]
+            dictionary[key] = input("enter {}, e.g. {}\n".format(key, temp))
+            if not dictionary[key].strip():
+                dictionary[key] = temp
+        with open('config.yaml', 'w') as stream:
+            yaml.dump(dictionary, stream, default_flow_style=False)
+
 
 
 config = None
--- matrix_registration/constants.py
+++ matrix_registration/constants.py
@@ -4,3 +4,4 @@ import os
 __location__ = os.path.realpath(os.path.join(os.getcwd(),
                                 os.path.dirname(__file__)))
 WORD_LIST_PATH = os.path.join(__location__, 'wordlist.txt')
+CONFIG_SAMPLE_PATH = os.path.join(__location__, 'config.sample.yaml')
