0db4f74468426b6ca68c64772ca22a6bf78e6186
ZerataX <mail@zera.tax>
2019-04-16 17:24:18 +0200

    add 3.8 to setup.py

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ],
     entry_points="""
         [console_scripts]
