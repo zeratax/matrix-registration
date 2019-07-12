81593a7bcec0e9e559bd061df66bd720734a9a1f
ZerataX <mail@zera.tax>
2019-07-13 01:34:29 +0200

    check for config file in ~/.config

--- matrix_registration/constants.py
+++ matrix_registration/constants.py
@@ -3,9 +3,14 @@ import os
 import site
 import sys
 
+# Third-party imports...
+from appdirs import user_config_dir
+
 __location__ = os.path.realpath(os.path.join(os.getcwd(),
                                 os.path.dirname(__file__)))
 WORD_LIST_PATH = os.path.join(__location__, 'wordlist.txt')
 CONFIG_PATH1 = os.path.join(__location__, '../')
-CONFIG_PATH2 = os.path.join(site.USER_BASE, 'config/')
-CONFIG_PATH3 = os.path.join(sys.prefix, 'config/')
+CONFIG_PATH2 = os.path.join(sys.prefix, 'config/')
+CONFIG_PATH3 = os.path.join(user_config_dir('matrix-registration') + '/')
+CONFIG_PATH4 = os.path.join(site.USER_BASE, 'config/')
+
