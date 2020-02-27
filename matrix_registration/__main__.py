a6d2eb6b8801f3906fecc5d6879937b721440672
ZerataX <mail@zera.tax>
2020-02-27 17:46:52 +0100

    add main() function

--- matrix_registration/__main__.py
+++ matrix_registration/__main__.py
@@ -48,44 +48,46 @@ def status_token(args):
         #                  indent=4,
         #                  default=str))
         print(tokens.tokens)
+def main():
+    parser = argparse.ArgumentParser(
+        description='a token based matrix registration app',
+        prog='python -m matrix_registration')
 
+    # subparser
+    subparsers = parser.add_subparsers(
+                            help='sub-commands. for ex. \'gen -h\' ' +
+                            'for additional help')
 
-parser = argparse.ArgumentParser(
-    description='a token based matrix registration app',
-    prog='python -m matrix_registration')
+    # api-parser
+    parser_a = subparsers.add_parser('api', help='start as api')
+    parser_a.set_defaults(func=run_api)
 
-# subparser
-subparsers = parser.add_subparsers(
-                        help='sub-commands. for ex. \'gen -h\' ' +
-                        'for additional help')
+    # generate-parser
+    parser_g = subparsers.add_parser('gen',
+                                     help='generate new token. ' +
+                                     '-o onetime, -e expire date')
+    parser_g.add_argument('-o', '--one-time', action='store_true',
+                          help='make token one-time-useable')
+    parser_g.add_argument('-e', '--expire', type=str, default=None,
+                          help='expire date: DD.MM.YYYY')
+    parser_g.set_defaults(func=generate_token)
 
-# api-parser
-parser_a = subparsers.add_parser('api', help='start as api')
-parser_a.set_defaults(func=run_api)
+    # status-parser
+    parser_s = subparsers.add_parser('status',
+                                     help='view status or disable ' +
+                                     'token. -s status, -d disable, -l list')
+    parser_s.add_argument('-s', '--status', type=str, default=None,
+                          help='token status')
+    parser_s.add_argument('-l', '--list', action='store_true',
+                          help='list tokens')
+    parser_s.add_argument('-d', '--disable', type=str, default=None,
+                          help='disable token')
+    parser_s.set_defaults(func=status_token)
 
-# generate-parser
-parser_g = subparsers.add_parser('gen',
-                                 help='generate new token. ' +
-                                 '-o onetime, -e expire date')
-parser_g.add_argument('-o', '--one-time', action='store_true',
-                      help='make token one-time-useable')
-parser_g.add_argument('-e', '--expire', type=str, default=None,
-                      help='expire date: DD.MM.YYYY')
-parser_g.set_defaults(func=generate_token)
+    args = parser.parse_args()
+    logger.debug('called with args: %s' % args)
+    if 'func' in args:
+        args.func(args)
 
-# status-parser
-parser_s = subparsers.add_parser('status',
-                                 help='view status or disable ' +
-                                 'token. -s status, -d disable, -l list')
-parser_s.add_argument('-s', '--status', type=str, default=None,
-                      help='token status')
-parser_s.add_argument('-l', '--list', action='store_true',
-                      help='list tokens')
-parser_s.add_argument('-d', '--disable', type=str, default=None,
-                      help='disable token')
-parser_s.set_defaults(func=status_token)
-
-args = parser.parse_args()
-logger.debug('called with args: %s' % args)
-if 'func' in args:
-    args.func(args)
+if __name__ == '__main__':
+    main()
