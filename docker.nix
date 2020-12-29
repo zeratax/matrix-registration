b986c9c14c0738f302ac51a6edde54162e9a75eb
ZerataX <mail@zera.tax>
2020-12-30 00:56:36 +0100

    update dockerimage.yml

--- docker.nix
+++ docker.nix
@@ -4,14 +4,23 @@ let
   matrix-registration = import ./shell.nix {};
 
 in pkgs.dockerTools.buildImage {
-  name = "matrix-registration"; 
-  tag = tag; 
+  name = "matrix-registration";
+  tag = tag;
+  created = "now";
 
   contents = [ matrix-registration ];
 
+  runAsRoot = ''
+    #!${pkgs.runtimeShell}
+    mkdir /data
+    cp ${matrix-registration}/config/config.sample.yaml /data/
+  '';
+
   config = { 
-    Entrypoint = ["${matrix-registration}/bin/matrix-registration" ];
-    Cmd = ["--config-path=/data/config.yaml"];
+    Entrypoint = [
+      "${matrix-registration}/bin/matrix-registration"
+      "--config-path=/data/config.yaml"
+      ];
     WorkingDir = "/data";
     Volumes = {
       "/data" = {};
