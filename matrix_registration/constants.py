418713b26bfef026e1f5ff7abc5157c3a0717a06
xelantro <40129767+xelantro@users.noreply.github.com>
2021-08-01 23:34:19 +0200

    Update constants.py

--- matrix_registration/constants.py
+++ matrix_registration/constants.py
@@ -9,6 +9,7 @@ from appdirs import user_config_dir
 __location__ = os.path.realpath(os.path.join(os.getcwd(),
                                 os.path.dirname(__file__)))
 WORD_LIST_PATH = os.path.join(__location__, 'wordlist.txt')
+CONFIG_SCHEMA_PATH = os.path.join(__location__, 'config.schema.json')
 # first check in current working dir
 CONFIG_PATH1 = os.path.join(os.getcwd() + '/')
 CONFIG_PATH2 = os.path.join(os.getcwd() + '/config/')
@@ -17,4 +18,3 @@ CONFIG_PATH3 = os.path.join(user_config_dir('matrix-registration') + '/')
 # check at installed location
 CONFIG_PATH4 = os.path.join(__location__, '../')
 CONFIG_PATH5 = os.path.join(sys.prefix, 'config/')
-
