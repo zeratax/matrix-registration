01a60be09a289d4345139cc9fc81b969a47d422a
xelantro <40129767+xelantro@users.noreply.github.com>
2021-08-01 23:35:08 +0200

    Update config.py

--- matrix_registration/config.py
+++ matrix_registration/config.py
@@ -10,6 +10,7 @@ from jsonschema import validate, ValidationError
 
 # Local imports...
 from .constants import (
+    CONFIG_SCHEMA_PATH,
     CONFIG_PATH1,
     CONFIG_PATH2,
     CONFIG_PATH3,
@@ -23,7 +24,7 @@ CONFIG_PATHS = [
     CONFIG_PATH4,
     CONFIG_PATH5
 ]
-CONFIG_SCHEMA_NAME = "config.schema.json"
+
 CONFIG_SAMPLE_NAME = "config.sample.yaml"
 CONFIG_NAME = 'config.yaml'
 logger = logging.getLogger(__name__)
@@ -82,10 +83,9 @@ class Config:
             try:
                 with open(self.data, 'r') as stream:
                     dictionary = yaml.load(stream, Loader=yaml.SafeLoader)
-                schema = next((os.path.join(path, CONFIG_SCHEMA_NAME) for path in CONFIG_PATHS if os.path.isfile(path + CONFIG_SCHEMA_NAME)), None)
-                if not schema:
-                    sys.exit("Make sure you have "+CONFIG_SCHEMA_NAME)
-                with open(schema, 'r') as schemafile:
+                if not CONFIG_SCHEMA_PATH:
+                    sys.exit("Make sure you have "+CONFIG_SCHEMA_PATH)
+                with open(CONFIG_SCHEMA_PATH, 'r') as schemafile:
                     validate(dictionary, yaml.safe_load(schemafile))               
             except ValidationError as e:
                 sys.exit("Check you config and update it to the newest version! Do you have missing fields in your config.yaml?\n\nTraceback:\n"+str(e))
